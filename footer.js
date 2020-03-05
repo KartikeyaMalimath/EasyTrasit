7b2cd344dbe94f907ed8b6caa697c03a2c8cdf14
KartikeyaMalimath <kartikeyapm@gmail.com>
2020-03-06 00:50:46 +0530

    templates and folder init

---
+++ footer.js
@@ -0,0 +1,39 @@
+document.write(`
+
+<footer class="footer">
+    <div class="container-fluid">
+        <nav class="float-left">
+        <ul>
+            <li>
+            <a href="#">
+                Easy-Transit
+            </a>
+            </li>
+            <li>
+            <a href="#">
+                About Us
+            </a>
+            </li>
+            <li>
+            <a href="#">
+                Blog
+            </a>
+            </li>
+            <li>
+            <a href="#">
+                Licenses
+            </a>
+            </li>
+        </ul>
+        </nav>
+        <div class="copyright float-right">
+        &copy;
+        <script>
+            document.write(new Date().getFullYear())
+        </script>, made with <i class="material-icons">favorite</i> by
+        <a href="#" target="_blank">Team EasyTransit</a> for a better transport.
+        </div>
+    </div>
+    </footer>
+
+`);
